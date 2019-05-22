c4a25614d2336fdd91c967329b4225d6ea32bfb8
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-05-22 17:43:51 +0100

    Improved to include basic computer functions as well as memory registers

--- cpu_flashcards.py
+++ cpu_flashcards.py
@@ -5,23 +5,23 @@ from random import *
 def getNumberOfRounds():
     return int(input("How many times would you like to play? "))
 
-def askQuestion(chosenLayer):
+def askQuestion(chosenFlashcard):
     if random() < 0.5:        
-        userAnswer = input("Which CPU register does the following sentence " + 
-                    "describe\n'{0}'\n".format(chosenLayer.getDescription()))
+        userAnswer = input("What does the following sentence describe\n'{0}'\n".format(
+                            chosenFlashcard.getDescription()))
         return userAnswer
     
     userAnswer = input("The full name of {0} is the ____ \n".format(
-                    chosenLayer.getAcronym()))
+                    chosenFlashcard.getAcronym()))
     return userAnswer
 
-def checkAnswer(chosenLayer, userAnswer):
-    if chosenLayer.getName() == userAnswer.title():
+def checkAnswer(chosenFlashcard, userAnswer):
+    if chosenFlashcard.getName() == userAnswer.title():
         return True
     return False
 
-def printResult(chosenLayer, response, score, i):
-    print("The correct answer was {0} \n".format(chosenLayer.getName()) + 
+def printResult(chosenFlashcard, response, score, i):
+    print("The correct answer was {0} \n".format(chosenFlashcard.getName()) + 
         "It seems you are {0} \nYour current score is {1}/{2} \n".format(
         response, score, i + 1))
 
@@ -35,17 +35,39 @@ def createCPURegisters():
         Register("AC", "Accumulator", "Temporarily stores data whilst calculations are being performed within the ALU")
     ]
 
-def main():
-    cpuRegisters = createCPURegisters()
+def createComputerFunctions():
+    return [
+        Register("DP Function", "Data Processing", "Completes the instructions given from the 'control'"),
+        Register("DS Function", "Data Storage", "Retains information for either short or long term periods for future use"),
+        Register("DM Function", "Data Movement", "Loads and stores data from on place to another"),
+        Register("C Function", "Control", "Runs the overall process of computer functions")
+    ]
+
+def playFlashcards(rounds, chosenFlashcard):
     score = 0
-    rounds = getNumberOfRounds()
     for i in range(rounds):
-        chosenLayer = choice(cpuRegisters)
-        userAnswer = askQuestion(chosenLayer)
+        userAnswer = askQuestion(chosenFlashcard)
+        if (userAnswer == "EXIT"): 
+            break
         response = "wrong :("
-        if checkAnswer(chosenLayer, userAnswer):
+        if checkAnswer(chosenFlashcard, userAnswer):
             response = "correct!"
             score += 1
-        printResult(chosenLayer, response, score, i)
+        printResult(chosenFlashcard, response, score, i)
+    print("\n\nYou ended with a score of {0}/{1}".format(score, i))
 
-main()
+def memoryRegisters():
+    cpuRegisters = createCPURegisters()
+    rounds = getNumberOfRounds()
+    playFlashcards(rounds, choice(cpuRegisters))
+
+def computerFunctions():
+    computerFunctions = createComputerFunctions()
+    rounds = getNumberOfRounds()
+    playFlashcards(rounds, choice(computerFunctions))
+
+def combinedQuestions():
+    computerFunctions = createComputerFunctions()
+    cpuRegisters = createCPURegisters()
+    rounds = getNumberOfRounds()
+    playFlashcards(rounds, choice([choice(computerFunctions), choice(cpuRegisters)]))
